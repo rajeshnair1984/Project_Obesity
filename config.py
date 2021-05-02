cea38e8afce92c1618b45eb85037a65ec791f520
rajeshnair1984 <rajeshnair812003@gmail.com>
2021-05-02 21:20:16 +0800

    newtransport branch and updates

--- config.py
+++ config.py
@@ -1,2 +1,21 @@
 weather_key = "273f91442c966dbca2945393b9e9390d"
 g_key = "AIzaSyAsKuDwzTZOEs67DVsnwgC6dRHr-LcX3D8"
+
+
+# mystring = 'This is a test.'
+
+# for i in range(len(mystring)):
+#     print (mystring[i])
+
+# mystring_list = [mystring[x] for x in range(len(mystring))]
+# print(mystring_list)
+
+tuple_1 = (1, 'Hyderabad', 13.5, True)
+tuple_2 = (11, 22, 33)
+tuple_2 = tuple_2 + (44,)
+
+print(sorted(tuple_2))
+def area_vol_cube(side_length):
+    area = 6 * side_length * side_length
+    volume = side_length * side_length * side_length
+    return area, volume
